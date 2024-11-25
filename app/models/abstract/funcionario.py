8a21aa141c65f97a5092135a0c93b6319102527f
StiveLucas <161142347+StiveLucas@users.noreply.github.com>
2024-11-25 22:56:09 +0000

    Atividade Em Equipe

--- app/models/abstract/funcionario.py
+++ app/models/abstract/funcionario.py
@@ -1,16 +1,17 @@
 import os
+from app.models.endereco import Endereco
 
-class Funcionario():
-    def __init__(self, nome: str, telefone: str, email: str, Endereco: Endereco) -> None:
+class Funcionario(Endereco):
+    def __init__(self, nome: str, telefone: str, email: str, salario: float, endereco: Endereco) -> None:
         self.nome = nome
         self.telefone = telefone
         self.email = email
-        self.Endereco = Endereco
-
-
-
-    def calcular_bonus(self) -> float:
-        pass
+        self.salario = salario
+        self.endereco = endereco
 
     def __str__(self) -> str:
-        return f"Funcionário(Nome: {self.nome}, Telefone: {self.telefone}, Email: {self.email}, Endereco: {self.Endereco})"
+        return(
+
+            f"\nFuncionário: \nNome: {self.nome}, \nTelefone: {self.telefone}"
+            f" \nEmail: {self.email} \nSalário: {self.salario} \nEndereco: {self.Endereco}"
+        ) 
