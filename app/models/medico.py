d29134d1fc467cd684a4daf32d181d86d2e6bd7e
Tijolinho529 <161033643+Tijolinho529@users.noreply.github.com>
2024-11-25 23:40:40 +0000

    atividade-em-equipee

---
+++ app/models/medico.py
@@ -0,0 +1,9 @@
+import os
+os.system("clear")
+
+class Médico():
+    def __init__(self, crm: str) -> None:
+        self.crm = crm
+
+    def __str__(self) -> str:
+      return(f"\nCrm: {self.crm}") 
