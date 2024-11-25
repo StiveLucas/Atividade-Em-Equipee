fb98b2931d3cf5786d77ab65afe3a8480716334b
Thiago Gabriel Torres <161034897+Thiago45367@users.noreply.github.com>
2024-11-25 22:27:13 +0000

    TrabalhoDupla

---
+++ app/models/abstract/funcionario.py
@@ -0,0 +1,16 @@
+import os
+
+class Funcionario():
+    def __init__(self, cpf: str, rg: str, matricula: str, setor: Setor, salario: float) -> None:
+        self.cpf = cpf
+        self.rg = rg
+        self.matricula = matricula
+        self.setor = setor
+        self.salario = salario
+
+
+    def calcular_bonus(self) -> float:
+        pass
+
+    def __str__(self) -> str:
+        return f"Funcionário(Matrícula: {self.matricula}, Setor: {self.setor}, Salário: R${self.salario:.2f})"
