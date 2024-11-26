b0069ac6b8159e9f9007f1521e737ccab7a1245d
StiveLucas <161142347+StiveLucas@users.noreply.github.com>
2024-11-26 00:08:01 +0000

    Atividade Em Enquipe

--- app/models/engenheiro.py
+++ app/models/engenheiro.py
@@ -1,9 +1,17 @@
 import os
+from app.models.abstract.funcionario import Funcionario
+from app.models.endereco import Endereco
 os.system("clear")
 
-class Engenheiro():
-    def __init__(self, crea: str) -> None:
-        self.crea = crea
+class Engenheiro(Funcionario):
+    def __init__(self, crea: str, nome: str, telefone: str, email: str, salario: float, endereco: Endereco) -> None:
+       super().__init__(nome, telefone, email, salario, endereco)
+
+       self.crea = crea
+
 
     def __str__(self) -> str:
-      return(f"\nCrea: {self.crea}") 
+      return(
+
+        f"\nNome: {self.nome}"
+      )
